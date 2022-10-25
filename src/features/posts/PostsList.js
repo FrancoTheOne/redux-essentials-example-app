b744c4aea5b716b6a10b9fc156f4358e1522ef5a
Franco Lau <fra@cccdi.hk>
2022-10-25 18:23:28 +0800

    Part 6: Normalizing Data

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,12 +2,13 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
-import { fetchPosts, selectAllPosts } from './postsSlice'
+import { fetchPosts, selectPostById, selectPostIds } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
 import { TimeAgo } from './TimeAgo'
 import {Spinner}  from '../../components/Spinner'
 
-const PostExcerpt = ({ post }) => {
+let PostExcerpt = ({ postId }) => {
+  const post = useSelector(state => selectPostById(state, postId))
   return (
     <article className="post-excerpt">
       <h3>{post.title}</h3>
@@ -24,11 +25,11 @@ const PostExcerpt = ({ post }) => {
     </article>
   )
 }
-
+// PostExcerpt = React.memo(PostExcerpt)
 
 export const PostsList = () => {
   const dispatch = useDispatch()
-  const posts = useSelector(selectAllPosts)
+  const orderedPostIds= useSelector(selectPostIds)
   
   const postStatus = useSelector(state => state.posts.status)
   const error = useSelector(state => state.posts.error)
@@ -44,25 +45,12 @@ export const PostsList = () => {
   if (postStatus === 'loading') {
     content = <Spinner text="Loading..." />
   } else if (postStatus === 'succeeded') {
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
-    content = orderedPosts.map(post => (
-      <PostExcerpt key={post.id} post={post} />
+    content = orderedPostIds.map(postId => (
+      <PostExcerpt key={postId} postId={postId} />
     ))
   } else if (postStatus === 'failed') {
     content = <div>{error}</div>
   }
-
-  // const renderedPosts = orderedPost.map((post) => (
-  //   <article className="post-excerpt" key={post.id}>
-  //     <h3>{post.title}</h3>
-  //     <PostAuthor userId={post.user} />
-  //     <p className="post-content">{post.content.substring(0, 100)}</p>
-  //     <ReactionButtons post={post} />
-  //     <Link to={`/posts/${post.id}`} className="button muted-button">
-  //       View Post
-  //     </Link>
-  //   </article>
-  // ))
 
   return (
     <section className="posts-list">
