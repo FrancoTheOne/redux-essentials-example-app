5f41fad0d65fd6f7ecae24b8134638af886f6bdf
Franco Lau <fra@cccdi.hk>
2022-10-28 15:34:43 +0800

    Complete Part 7

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,34 +1,32 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useGetPostQuery } from '../api/apiSlice'
 import { PostAuthor } from './PostAuthor'
-import { selectPostById } from './postsSlice'
 import { ReactionButtons } from './ReactionButtons'
+import { Spinner } from '../../components/Spinner'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
 
-  const post = useSelector(state => selectPostById(state, postId))
+  const { data: post, isFetching, isSuccess } = useGetPostQuery(postId)
+  // const post = useSelector(state => selectPostById(state, postId))
 
-  if (!post) {
-    return (
-      <section>
-        <h2>Post not found!</h2>
-      </section>
-    )
-  }
-
-  return (
-    <section>
+  let content
+  if (isFetching) {
+    content = <Spinner text="Loading..." />
+  } else if (isSuccess) {
+    content = (
       <article className="post">
         <h2>{post.title}</h2>
         <PostAuthor userId={post.user} />
         <p className="post-content">{post.content}</p>
         <ReactionButtons post={post} />
-        <Link to={`/editPost/${post.id}`} className='button'>
+        <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
         </Link>
       </article>
-    </section>
-  )
+    )
+  }
+
+  return <section>{content}</section>
 }
